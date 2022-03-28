45af05906f6bf55663cd8d3cbd987eed1ea68bb4
gibran.kichlu <gibran.kichlu@gmail.com>
2022-03-28 09:39:48 -0400

    add kiosk scripts

---
+++ kiosk/kiosk-local.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# offline version
+xset s noblank
+xset s off
+xset -dpms
+
+unclutter -idle 0.5 -root &
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+# start node http server
+http-server ~/salattime/dist/ &
+
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8080
+
+# while true; do
+#      xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
+#      sleep 15
+# done
+
+
---
+++ kiosk/kiosk.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+xset s noblank
+xset s off
+xset -dpms
+
+unclutter -idle 0.5 -root &
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://playful-cendol-04166c.netlify.app
+
+# while true; do
+#      xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
+#      sleep 15
+# done
+
+
