b7d7ddb861b16377e3ed6775e67a68d2e564cbdb
gibran.kichlu <gibran.kichlu@gmail.com>
2022-04-13 06:13:17 -0400

    new kiosk website links

--- kiosk/kiosk.sh
+++ kiosk/kiosk.sh
@@ -8,16 +8,12 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://playful-cendol-04166c.netlify.app
-
-# while true; do
-#      xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
-#      sleep 15
-# done
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://ahlehadithcanada.org/prayer-times-digital-screen/    https://ahlehadithcanada.org/important-announcements/
 
 while true; do
-     xdotool keydown F5; xdotool keyup F5;
-     sleep 5
+     xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
+     sleep 15
 done
+
 
 
---
+++ kiosk/readme.md
@@ -0,0 +1,2 @@
+https://ahlehadithcanada.org/prayer-times-digital-screen/
+https://ahlehadithcanada.org/important-announcements/
